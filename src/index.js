4546fa74c917d8d65c10d0840abebec875428a50
Idan Goldman <idan@goldman.work>
2018-11-10 00:27:15 +0200

    init commit 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+String.prototype.sayHello = function sayHello() {
+  return `Hello ${this}!`;
+};
+
+String.prototype.capitalize = function capitalize() {
+  return this[0].toUpperCase() + this.slice(1).toLowerCase();
+};
+
+// export const zeroPad = ( number, prefix = true ) => {
+//   return number <= 9 ? prefix ? '0' + number : number + '0' : number.toString();
+// };
+//
+//
+// export const snakeToCamel = ( string ) => {
+//   return string.replace( /(\-\w)/g, match => match[1].toUpperCase() );
+// };
